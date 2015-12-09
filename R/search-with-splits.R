28507fea69fc654b63fe58181ebe31e5538a96c2
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2015-12-09 17:06:05 -0600

    Debugged A* and added stochastic tie-breaking to it.

--- R/search-with-splits.R
+++ R/search-with-splits.R
@@ -5,7 +5,7 @@
 #' 
 #' This version looks for the best actor ancestry individually per reporter and then
 #' composes.
-searchWithSplits = function( laps, searchFunction = searchByAStar){
+searchWithSplits = function( laps, searchFunction = searchByAStar ){
   ancestry.template = matrix(
     FALSE,
     nrow = howManyActors( laps ),
@@ -25,7 +25,16 @@ searchWithSplits = function( laps, searchFunction = searchByAStar){
 #' Search for a reporter's best network
 #' 
 #' This version is a variation on A* search
-searchByAStar = function( laps, reporterIndex ){
+#' 
+#' @param laps the Local Ancestry Probablity Score object
+#' @param reporterIndex the index of the reporter for which we're building the network
+#' @param searchSelection function determining the search order, by default using lastMax,
+#' which corresponds to DFS A*. An interesting alternative is choosing a random max,
+#' choosing the first max will yield the abysmally slow BFS A*.
+searchByAStar = function( laps, reporterIndex, searchSelection = whichLastMax ){
+  
+  # For debugging
+  print( reporterIndex )
   
   n = howManyActors( laps )
   
@@ -43,18 +52,30 @@ searchByAStar = function( laps, reporterIndex ){
   
   repeat {
     # Find and "pop" best scoring point
-    index = which.max(boundary.scores)
+    index = searchSelection( boundary.scores )
     score = boundary.scores[ index ]
     state = boundary[[ index ]]
     boundary = boundary[ -index ]
     boundary.scores = boundary.scores[ -index ]
     
-    if ( state$depth == maxDepth ){
-      ancestry = deriveAncestry(
-        state$adjacency,
-        matrix( FALSE, nrow = n, ncol = n + 1 ) )$ancestry
+    # For debugging
+    #print('Boundary:')
+    #print(boundary.scores)
+    #print('Depth:')
+    #print(state$depth)
+    
+    uncertainAncestry = deriveAncestry(
+      state$adjacency,
+      getUncertaintyMatrix( state$depth, n, n+1 ) )
+    
+    if ( state$depth == maxDepth || !any( uncertainAncestry$uncertain[,n+1] ) ){
+      ancestry = uncertainAncestry$ancestry
       break
     }
+    
+    # For debugging
+    #print('Matrix:')
+    #print( showUncertainAncestry( uncertainAncestry$ancestry, uncertainAncestry$uncertain ) )
     
     # We can reuse index as our insertion point now
     index = length( boundary ) + 1
@@ -66,28 +87,80 @@ searchByAStar = function( laps, reporterIndex ){
     new.adjacency = state$adjacency
     new.adjacency[ toggle.point ] = TRUE
     new.state = list( depth = state$depth + 1, adjacency = new.adjacency )
-    # "push" new state in.
-    boundary[ index ] = list( new.state )
-    boundary.scores[ index ] = getHeuristicScore(
-      laps,
-      reporterIndex,
-      state$depth + 1,
-      new.adjacency )
+    # pruning and scoring
+    if( -Inf < ( new.score = getHeuristicScore( laps,
+                                                reporterIndex,
+                                                state$depth + 1,
+                                                new.adjacency ) ) ) {
+      # "push" new state in.
+      boundary[ index ] = list( new.state )
+      boundary.scores[ index ] = new.score
+      index = index + 1
+    }
     
-    index = index + 1
     # Compute "edge absent"
     new.adjacency[ toggle.point ] = FALSE
     new.state = list( depth = state$depth + 1, adjacency = new.adjacency )
     # "push" new state in.
-    boundary[ index ] = list( new.state )
-    boundary.scores[ index ] = getHeuristicScore(
-      laps,
-      reporterIndex,
-      state$depth + 1,
-      new.adjacency )
+    if( -Inf < ( new.score = getHeuristicScore( laps,
+                                                reporterIndex,
+                                                state$depth + 1,
+                                                new.adjacency ) ) ) {
+      # "push" new state in.
+      boundary[ index ] = list( new.state )
+      boundary.scores[ index ] = new.score
+    }
   }
   
+  # For debugging
+  print( score )
+  
   return ( list( score = score, ancestry = ancestry ) )
+}
+
+#' Given a vector, get the index of the last maximal element
+#' 
+#' @param a vector of numbers
+#' @return the index of the last maximal element
+whichLastMax = function ( x ){
+  z = which( x == max( x ) )
+  z[length(z)]
+}
+
+#' Given a vector, get the index of a random maximal element
+#' 
+#' Given a vector, looks at its maximal elements, walking from last to first,
+#' rejecting each with probability (1-p). (So has probability p to pick the last,
+#' p*(1-p)th the 2nd last, etc. with the remainder of the probabilty mass placed
+#' in the first).
+#' 
+#' @param a vector of numbers
+#' @return the index of a (non-uniformly) random maximal element
+whichRandomLateMax = function ( x, p = 2/3 ){
+  z = which( x == max( x ) )
+  i = length(z)
+  while( sample( x = c( TRUE, FALSE ), size = 1, prob = c( 1-p, p ) ) && i > 1 ){
+    i = i-1
+  }
+  z[i]
+}
+
+#' Make uncertainty matrix from depth + dimension
+#' 
+#' @param depth - search depth
+#' @param n - number of rows
+#' @param m - number of columns
+getUncertaintyMatrix = function ( depth = 0, n, m = n ){
+  
+  # Diagonals are never uncertain.
+  uncertain = ( diag( nrow = n, ncol = m ) == 0 )
+  
+  if( depth > 0 ){
+    indeces = which( uncertain )
+    uncertain[ indeces[ length( indeces ) - ( ( depth - 1 ):0 ) ] ] = FALSE
+  }
+  
+  return ( uncertain )
 }
 
 #' The heuristic + score for A*
@@ -96,12 +169,7 @@ getHeuristicScore = function ( laps, reporterIndex, depth, adjacency ){
   n = nrow( adjacency )
   
   # Mark certain/free edges in adjacency matrix.
-  uncertain = cbind( diag( n ) == 0, TRUE )
-  
-  if( depth > 0 ){
-    indeces = which( uncertain )
-    uncertain[ indeces[ length( indeces ) - ( ( depth - 1 ):0 ) ] ] = FALSE
-  }
+  uncertain = getUncertaintyMatrix( depth, n, n+1 )
   
   #print( uncertain )
   
@@ -109,9 +177,9 @@ getHeuristicScore = function ( laps, reporterIndex, depth, adjacency ){
   ancestral = deriveAncestry( adjacency, uncertain )
   uncertain = ancestral$uncertain
   ancestry = ancestral$ancestry
-  
-  #print( uncertain )
-  #print( ancestry )
+
+  # For debugging  
+  #print( showUncertainAncestry( ancestry, uncertain ) )
   
   # Score
   
@@ -155,6 +223,9 @@ getHeuristicScore = function ( laps, reporterIndex, depth, adjacency ){
     }
   }
   
+  # For debugging
+  #print( score )
+  
   return ( score )
 }
 
@@ -195,4 +266,17 @@ deriveAncestry = function ( adjacency, uncertain ){
   updated.uncertain[updated.ancestry] = FALSE
   
   return ( list( ancestry = updated.ancestry[1:n,1:m], uncertain = updated.uncertain[1:n,1:m] ) )
+}
+
+#' A human-friendly representation of ancestry+uncertainty matrix
+#' 
+#' @param ancestry the ancesty matrix
+#' @param uncertainty the uncertainty matrix
+#' @return a char matrix with T/F in certain cells and ? in uncertain cells.
+showUncertainAncestry = function ( ancestry, uncertainty ){
+  dims = dim( ancestry )
+  result = matrix( '?', nrow = dims[1], ncol = dims[2] )
+  result[ ancestry & !uncertainty ] = 'T'
+  result[ !ancestry & !uncertainty ] = 'F'
+  return ( result )
 }
