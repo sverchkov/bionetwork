8a27f6c418825ca649dd9cd14a2bfa7ec22ecd91
Yuriy Sverchkov <yuriy.sverchkov@wisc.edu>
2016-01-07 22:00:39 -0600

    Fixed aggregator

--- R/ancestry-aggregation.R
+++ R/ancestry-aggregation.R
@@ -7,6 +7,7 @@
 #' reporter-ancestries.
 #' @return a single ancestry (with possibly duplicated actors) which is a matrix of
 #' size (new actors) x (new actors + reporters)
+#' @export
 aggregateAncestries = function ( ancestries ){
   
   # Get dimensions and names
@@ -15,14 +16,14 @@ aggregateAncestries = function ( ancestries ){
   nR = ds[3]
   
   dn = dimnames( ancestries )
+  
+  actorMap = vector( mode = "character", length = 0 )
+  actors = 1:nA
+  reporters = 1:nR
+  
   if ( ! is.null( dn ) ){
     actors = dn[[ 1 ]]
     reporters = dn[[ 3 ]]
-    actorMap = vector( mode = "character", length = 0 )
-  } else {
-    actors = 1:nA
-    reporters = 1:nR
-    actorMap = vector( mode = "numeric", length = 0 )
   }  
   
   # An indexing system to keep track of actor copies
@@ -39,19 +40,17 @@ aggregateAncestries = function ( ancestries ){
     # Get its ancestors
     ancestry = ancestries[, nA+1, reporter ]
     
-    for ( actor in actors( ancestry ) ) { # For each ancestor
+    for ( actor in actors[ ancestry ] ) { # For each ancestor
       
       actor.ancestors = ancestries[, actor, reporter]
       
-      # Check if it is already in the version list
+      # Add if it is not already in the version list
       version.matrix = actor.versions[[ actor ]]$matrix
-      v = which( apply( version.matrix == actor.ancestors, 2, all ) )
-      if ( v == 0 ) { # If not, add it
-        v = dim( version.matrix )[ 2 ] + 1
-        version.matrix[, v ] = actor.ancestors
+      if ( length( version.matrix ) < 1 || ! any( apply( version.matrix == actor.ancestors, 2, all ) ) ){
+        version.matrix = cbind( version.matrix, actor.ancestors )
         actor.versions[[ actor ]]$matrix = version.matrix
         numberOfActorNodes = numberOfActorNodes + 1
-        actor.versions[[ actor ]]$indeces[v] = numberOfActorNodes
+        actor.versions[[ actor ]]$indeces = c( actor.versions[[ actor ]]$indeces, numberOfActorNodes )
       }
     }
   }
@@ -79,7 +78,7 @@ aggregateAncestries = function ( ancestries ){
     # Get its ancestors
     ancestry = ancestries[, nA+1, reporter ]
     
-    for ( actor in actors( ancestry ) ) { # For each ancestor
+    for ( actor in actors[ ancestry ] ) { # For each ancestor
       
       actor.ancestors = ancestries[, actor, reporter]
       
@@ -88,7 +87,7 @@ aggregateAncestries = function ( ancestries ){
       
       result.ancestry[ i, reporter ] = TRUE
       
-      for ( ancestor in actors( actor.ancestors ) ){
+      for ( ancestor in actors[ actor.ancestors ] ){
         
         # Get the ancestor index
         j = actor.versions[[ ancestor ]]$indeces[ which( apply( actor.versions[[ ancestor ]]$matrix == ancestries[, ancestor, reporter], 2, all ) ) ]
